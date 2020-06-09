0aef7ce60133bc79410b49f69a66b98afde01d6d
Barry Wu <barryxwu@gmail.com>
2020-06-09 20:42:56 +1000

    undo commit to test git global config

--- src/index.js
+++ src/index.js
@@ -7,6 +7,19 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000
 
+
+// // test middleware
+// const jwt = require('jsonwebtoken')
+// const testFunction = async () => {
+//     const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZWQxYmU3ZTZjZjg0NjI5ZDBjNTBjZmYiLCJpYXQiOjE1OTA4MDQwOTZ9.dvR0aEew28Ir8WsubVXOcRbTKYMgH-ZtPXfD8-a8EwU'
+//     const tokenValid = jwt.verify(token,'secretstring')
+//     return tokenValid
+// }
+
+// testFunction()
+//     .then((tokenValid) => console.log(tokenValid))
+//     .catch(e => console.log(e.message))
+
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
